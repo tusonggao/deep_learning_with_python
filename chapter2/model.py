6715e86bb71b50c3c0acf9dd82c4df5701911703
tusonggao <tusonggao@163.com>
2020-01-20 18:15:51 +0800

    update files

--- chapter2/model.py
+++ chapter2/model.py
@@ -7,4 +7,11 @@ import pandas as pd
 
 import keras
 
+from keras.datasets import mnist
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+print('train_images.shape is ', train_images.shape, 'len of train_labels is ', len(train_labels))
+print('test_images.shape is ', test_images.shape, 'len of test_labels is ', len(test_labels))
+
 
