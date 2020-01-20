ce0b60879fd098bf448a2656613ecd41d956aa28
tusonggao <tusonggao@163.com>
2020-01-20 18:20:36 +0800

    update files

--- chapter2/model.py
+++ chapter2/model.py
@@ -14,4 +14,32 @@ from keras.datasets import mnist
 print('train_images.shape is ', train_images.shape, 'len of train_labels is ', len(train_labels))
 print('test_images.shape is ', test_images.shape, 'len of test_labels is ', len(test_labels))
 
+from keras import models
+from keras import layers
+
+network = models.Sequential()
+network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
+network.add(layers.Dense(10, activation='softmax'))
+
+network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+print('prog starts here!')
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+
+from keras.utils import to_categorical
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+print('test_acc:', test_acc)
+
+print('prog ends here!')
+
+
+
 
